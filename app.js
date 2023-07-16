191c2f180ee0660849b28a1d269bd86376e8277f
Himanshu Rajput <hiowner00@gmail.com>
2023-07-16 15:20:36 +0530

    local url added

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
     
-    origin: [process.env.FRONTEND_URL],
+    origin: [process.env.FRONTEND_URL, process.env.LOCAL_URI],
     methods: ["GET","PUT", "POST", "DELETE"],
     credentials: true
 }))
